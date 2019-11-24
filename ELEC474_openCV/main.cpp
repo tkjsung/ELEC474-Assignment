670954d361d3d1f2c2b73b88a856609b06808089
Ke Jun (Tom) Sung <tomssung@gmail.com>
2019-11-23 23:39:55 -0500

    Testing for loop... not working

--- ELEC474_openCV/main.cpp
+++ ELEC474_openCV/main.cpp
@@ -34,6 +34,7 @@ vector<string>listOfWLHImages = getImages("WLH/*.jpg");
 //vector<Mat> matChurch = loadImages(listOfChurchImages);
 //vector<Mat> matOffice = loadImages(listOfOfficeImages);
 //vector<Mat> matwlh = loadImages(listOfWLHImages);
+Mat pano;
 
 int main()
 {
@@ -80,13 +81,14 @@ void imgORBMatch(String identify)
         cout << "Error occurred" << endl;
         return;
     }
-    Mat img1 = matSource[0];
-    Mat img2 = matSource[1];
     
+    // All variable declarations
+    Mat img1, img2, descriptors1, descriptors2, h, matchesMatrix, resultImg;
     vector<KeyPoint> keypoints1, keypoints2;
-    Mat descriptors1, descriptors2;
     vector<Point2f> img1Points, img2Points;
-    
+    vector<DMatch> matches12, matches21, filteredMatches12, filteredMatches21;
+    Ptr<FeatureDetector> detector;
+    Ptr<DescriptorMatcher> matcher;
     int nfeatures = 500;
     float scaleFactor = 1.2f;
     int nlevels = 8;
@@ -94,95 +96,112 @@ void imgORBMatch(String identify)
     int firstLevel = 0;
     int WTA_K = 2;
     int patchSize = 31;
+    int j = 1;
+    int first = 0;
     
-    // Feature Detector
-    Ptr<FeatureDetector> detector = ORB::create(nfeatures, scaleFactor, nlevels, edgeThreshold, firstLevel, WTA_K, ORB::HARRIS_SCORE, patchSize);
-    detector->detectAndCompute(img1, Mat(), keypoints1, descriptors1);
-    detector->detectAndCompute(img2, Mat(), keypoints2, descriptors2);
+//    cout << matSource.size() << endl;
     
-    // Match features
-    vector<DMatch> matches12, matches21, filteredMatches12, filteredMatches21;
-    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
-    matcher->match(descriptors1, descriptors2, matches12, Mat());
-    matcher->match(descriptors2, descriptors1, matches21, Mat());
-
-    
-    // Sort matches to delete the matches that aren't so great
-//    sort(matches12.begin(), matches12.end());
-    // Remove not good matches
-//    const int numGoodMatches = matches12.size() * 0.15f;
-//    matches12.erase(matches.begin() + numGoodMatches, matches12.end());
-    
-    // From : https://answers.opencv.org/question/15/how-to-get-good-matches-from-the-orb-feature-detection-algorithm/
-    for (size_t i = 0; i < matches12.size(); i++)
+    for (j = 1; j < matSource.size(); j++)
     {
-        DMatch forward = matches12[i];
-        DMatch backward = matches21[forward.trainIdx];
-        if(backward.trainIdx == forward.queryIdx)
-            filteredMatches12.push_back(forward);
+        if (first == 0)
+        {
+            img1 = matSource[0];
+            img2 = matSource[1];
+            first = 1;
+        }
+        else{
+            img1 = pano;
+            img2 = matSource[j];
+        }
+        
+        
+        // Feature Detector
+        detector = ORB::create(nfeatures, scaleFactor, nlevels, edgeThreshold, firstLevel, WTA_K, ORB::HARRIS_SCORE, patchSize);
+        detector->detectAndCompute(img1, Mat(), keypoints1, descriptors1);
+        detector->detectAndCompute(img2, Mat(), keypoints2, descriptors2);
+        
+        // Match features
+        //vector<DMatch> matches12, matches21, filteredMatches12, filteredMatches21;
+        matcher = DescriptorMatcher::create("BruteForce-Hamming");
+        matcher->match(descriptors1, descriptors2, matches12, Mat());
+        matcher->match(descriptors2, descriptors1, matches21, Mat());
+        
+        // Sort matches to delete the matches that aren't so great
+//        sort(matches12.begin(), matches12.end());
+        // Remove not good matches
+//        const int numGoodMatches = matches12.size() * 0.05f;
+//        matches12.erase(matches12.begin() + numGoodMatches, matches12.end());
+        
+        // From : https://answers.opencv.org/question/15/how-to-get-good-matches-from-the-orb-feature-detection-algorithm/
+        for (size_t i = 0; i < matches12.size(); i++)
+        {
+            DMatch forward = matches12[i];
+            DMatch backward = matches21[forward.trainIdx];
+            if(backward.trainIdx == forward.queryIdx)
+                filteredMatches12.push_back(forward);
+        }
+        
+        // This for statement might not be needed...
+        //    for (size_t i = 0; i < matches21.size(); i++)
+        //    {
+        //        DMatch forward = matches21[i];
+        //        DMatch backward = matches21[forward.trainIdx];
+        //        if(backward.trainIdx == forward.queryIdx)
+        //            filteredMatches21.push_back(forward);
+        //    }
+        
+        for (size_t i = 0; i < filteredMatches12.size(); i++)
+        {
+            img1Points.push_back(keypoints1[filteredMatches12[i].queryIdx].pt);
+            img2Points.push_back(keypoints2[filteredMatches12[i].trainIdx].pt);
+        }
+        
+        // Find homography (source pts, dst pts, algorithm)
+        h = findHomography(img2Points, img1Points, RANSAC);
+        
+        
+        // Draw Matches algorithm
+        drawMatches(img1, keypoints1, img2, keypoints2, filteredMatches12, matchesMatrix);
+        imshow("Matches",matchesMatrix);
+        waitKey();
+        
+        //    cout << img1.size[0] << endl; // height
+        //    cout << img1.size[1] << endl; // width
+        //    cout << img1.size << endl;
+        
+        // Warp image according to the homography
+//        warpPerspective(img2, pano, h, Size((img1.rows + img2.rows),(img1.cols + img2.cols)));
+//        Mat half(pano, Rect(0,0,img1.cols,img1.rows));
+//        img1.copyTo(half);
+        
+        warpPerspective(img2, resultImg, h, Size((img1.rows + img2.rows),(img1.cols + img2.cols)));
+        pano = Mat(resultImg, Rect(0,0,img1.cols,img1.rows));
+        img1.copyTo(pano);
+        
+        imshow("Perspective change",resultImg);
+        waitKey();
+        imshow("Changed image",pano);
+        waitKey();
+        
+        
+        // Clear all variables
+        keypoints1.clear();
+        keypoints2.clear();
+        img1Points.clear();
+        img2Points.clear();
+        descriptors1.release();
+        descriptors2.release();
+        h.release();
+        matchesMatrix.release();
+        resultImg.release();
+        matches12.clear();
+        matches21.clear();
+        filteredMatches12.clear();
+        filteredMatches21.clear();
+//        half.release();
+        detector->clear();
+        matcher->clear();
     }
-    
-    // This for statement might not be needed...
-    for (size_t i = 0; i < matches21.size(); i++)
-    {
-        DMatch forward = matches21[i];
-        DMatch backward = matches21[forward.trainIdx];
-        if(backward.trainIdx == forward.queryIdx)
-            filteredMatches21.push_back(forward);
-    }
-
-    for (size_t i = 0; i < filteredMatches12.size(); i++)
-    {
-        img1Points.push_back(keypoints1[filteredMatches12[i].queryIdx].pt);
-        img2Points.push_back(keypoints2[filteredMatches12[i].trainIdx].pt);
-    }
-    
-    // Find homography (source pts, dst pts, algorithm)
-    Mat h = findHomography(img2Points, img1Points, RANSAC);
-    
-    
-    // Draw Matches algorithm
-    Mat matchesMatrix;
-    drawMatches(img1, keypoints1, img2, keypoints2, filteredMatches12, matchesMatrix);
-    imshow("Matches",matchesMatrix);
-    waitKey();
-    
-//    cout << img1.size[0] << endl; // height
-//    cout << img1.size[1] << endl; // width
-//    cout << img1.size << endl;
-    
-    Mat resultImg;
-    
-    // Warp image according to the homography
-    warpPerspective(img2, resultImg, h, Size((img1.rows + img2.rows),(img1.cols + img2.cols)));
-    Mat half(resultImg, Rect(0,0,img1.cols,img1.rows));
-    img1.copyTo(half);
-    
-    
-//    cout << resultImg.type();
-    
-//    for(int r = 0; r < img1.size[0]; r++)
-//    {
-//        for(int c = 0; c < img1.size[1]; c++)
-//        {
-//            resultImg.at<unsigned char>(r,c) = img1.at<unsigned char>(r,c);
-//
-//        }
-////        cout << "column: " << c << endl;
-//        cout << "row: " << r << endl;
-//    }
-    
-    
-    imshow("Perspective change",resultImg);
-    waitKey();
-//    imshow("Changed image",half);
-//    waitKey();
-    
-    // Put all the image matrices into a vector to use stitcher tool... potentially.
-//    vector<Mat> resultImg;
-//    resultImg.push_back(img1);
-//    resultImg.push_back(matchesMatrix);
-    
     
 }
 
@@ -264,7 +283,6 @@ void imgSIFTMatch(String identify)
 //    cout << newKeypoints1.size() << endl;
 //
     
-    //TODO:: Transfer all code from SIFT back to ORB and continue development in ORB.
     for (size_t i = 0; i < filteredMatches12.size(); i++)
     {
         img1Points.push_back(keypoints1[filteredMatches12[i].queryIdx].pt);
